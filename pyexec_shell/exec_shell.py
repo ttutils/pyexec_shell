0bc2c24ccaab227c3f27e86935f6963b3955c4b5
buyfakett <buyfakett@vip.qq.com>
2024-06-30 09:09:13 +0800

    增加只检测一次

--- pyexec_shell/exec_shell.py
+++ pyexec_shell/exec_shell.py
@@ -24,32 +24,36 @@ def exec_shell(command: str):
     return output.decode('utf-8'), error.decode('utf-8'), process.returncode
 
 
-def check_file(directory: str, filename: str, max_checks: int = 60):
+def check_file(directory: str, filename: str, max_checks: int = 60, is_single_check: bool = False):
     """
     检测服务器是否有这个文件
     :param directory:           检查的目录
     :param filename:            检查的文件名
     :param max_checks:          最多检测的次数
+    :param is_single_check:     是否只检测一次
     """
-    checks = 1
-    logging.info(f'本次检查{directory}下的{filename}文件，一共检测{max_checks}次')
+    if is_single_check:
+        return os.path.isfile(directory + filename)
+    else:
+        checks = 1
+        logging.info(f'本次检查{directory}下的{filename}文件，一共检测{max_checks}次')
 
-    while checks < max_checks:
-        # 检测文件夹本身是否存在
-        if not os.path.exists(directory):
-            logging.info(f"当前为第{checks}次尝试，文件夹 {directory} 不存在，等待...")
-            time.sleep(5)
-            checks += 1
-        else:
-            # 检测文件是否存在
-            files = os.listdir(directory)
-            matched_files = [file for file in files if fnmatch.fnmatch(file, filename)]
-
-            if matched_files:
-                logging.info(f"当前为第{checks}次尝试，文件 {filename} 存在，继续执行！")
-                return True  # 如果文件存在，返回成功
-            else:
-                logging.info(f"当前为第{checks}次尝试，文件 {filename} 不存在，等待...")
+        while checks < max_checks:
+            # 检测文件夹本身是否存在
+            if not os.path.exists(directory):
+                logging.info(f"当前为第{checks}次尝试，文件夹 {directory} 不存在，等待...")
                 time.sleep(5)
                 checks += 1
-    return False  # 达到最大检测次数后返回失败
+            else:
+                # 检测文件是否存在
+                files = os.listdir(directory)
+                matched_files = [file for file in files if fnmatch.fnmatch(file, filename)]
+
+                if matched_files:
+                    logging.info(f"当前为第{checks}次尝试，文件 {filename} 存在，继续执行！")
+                    return True  # 如果文件存在，返回成功
+                else:
+                    logging.info(f"当前为第{checks}次尝试，文件 {filename} 不存在，等待...")
+                    time.sleep(5)
+                    checks += 1
+        return False  # 达到最大检测次数后返回失败
